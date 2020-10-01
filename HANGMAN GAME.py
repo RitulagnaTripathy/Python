4e63a8a637770ca5ce741e0ac98d91f9dfb6de04
RitulagnaTripathy <63919911+RitulagnaTripathy@users.noreply.github.com>
2020-10-01 11:50:45 +0530

    Add files via upload

---
+++ HANGMAN GAME.py
@@ -0,0 +1,65 @@
+#Implementation of Hangman game
+import random
+
+score=10
+ans=[]
+guess=''
+c=0
+
+name=input("What's ur name?")
+print("Hey"+name,",Let's play Hangman!!")
+#List of movies 
+movie_list=['LAGAAN','HIGHWAY','THOR','DANGAL','NEWTON']
+movie=random.choice(movie_list)
+a=len(movie) #no of letters in movie
+ans.extend(movie)
+for i in range(a):
+    ans[i]= '*'
+print(ans)
+#initial score=10
+print("score=10")
+while (score>0):
+    #to input the guessed letter
+    print("Guess a letter")
+    guess=input()
+    if guess in movie: #to check whether the guessed letter is correct or not
+       for i in range(a):
+            if guess in ans[i]:
+                print("U have already guessed it!")
+                break
+            if movie[i]==guess:
+                ans[i]=guess
+                c+=1
+                print(ans)
+                print("Correct guess!")
+                #3 points will be awarded for correct guess
+                score+=3
+                print(score) #to print score
+                
+    if guess not in movie:
+        print("Ooops!Wrong guess")
+        #2 points will be deducted for wrong guess
+        score-=2
+        print(score)#to print score
+    if c==a:
+        break
+
+for i in range(a): #declaration of final result
+    if ans[i]==movie[i]:
+        print("Congrats..you won!!")
+        break
+    else:
+        print("You lose!")
+        print("Better luck next time")
+        break
+        
+    
+
+
+    
+        
+        
+            
+            
+
+
